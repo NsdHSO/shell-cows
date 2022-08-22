f2c9f193f99d0eb4e8e47c2d15a281d6d6acaefe
NsdHSO (Nechiforel David-Samuel) <nechiforelsamuel@yahoo.com>
2022-08-23 01:04:36 +0300

    feat: added auth guard

--- apps/z-15/src/app/app.routing.ts
+++ apps/z-15/src/app/app.routing.ts
@@ -1,17 +1,12 @@
-import { AppComponent } from './app.component';
 import { Routes } from "@angular/router";
 import { loadRemoteModule } from '@angular-architects/module-federation';
-import { AuthorizationGuard } from 'ngx-virous';
-import {TestViewComponent} from "./test-view/test-view.component";
 import {environment} from "../environments/environment";
+import {ShellComponent} from "./shell/shell.component";
+import {AuthorizationGuard} from "ngx-virous";
 
 export const routes: Routes = [
     {
-        path: 'app',
-        component: AppComponent,
-    },
-    {
-        path:'login',
+        path:'',
         loadChildren: () => loadRemoteModule({
             type: 'module',
             remoteEntry: `${environment.mfe.login}remoteEntry.js`,
@@ -19,8 +14,8 @@ export const routes: Routes = [
         }).then(m => m.SignInModule),
     },
   {
-    path: 'test',
-    component: TestViewComponent,
+    path:'apps',
+    component: ShellComponent,
     canActivate: [AuthorizationGuard]
   }
 ]
--- apps/z-15/src/app/shell/shell.component.spec.ts
+++ apps/z-15/src/app/shell/shell.component.spec.ts
@@ -1,17 +1,17 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { TestOldComponent } from './test-old.component';
+import { ShellComponent } from './shell.component';
 
-describe('TestOldComponent', () => {
-  let component: TestOldComponent;
-  let fixture: ComponentFixture<TestOldComponent>;
+describe('ShellComponent', () => {
+  let component: ShellComponent;
+  let fixture: ComponentFixture<ShellComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [TestOldComponent],
+      declarations: [ShellComponent],
     }).compileComponents();
 
-    fixture = TestBed.createComponent(TestOldComponent);
+    fixture = TestBed.createComponent(ShellComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
