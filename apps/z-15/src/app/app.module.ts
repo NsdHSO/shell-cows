71c6ff14d742ca63772363159250048544a9d664
NsdHSO (Nechiforel David-Samuel) <nechiforelsamuel@yahoo.com>
2022-08-23 16:21:07 +0300

    feat: added animation

--- apps/z-15/src/app/app.module.ts
+++ apps/z-15/src/app/app.module.ts
@@ -8,13 +8,21 @@ import { FrameWholeModule } from 'ngx-liburg-frame-side';
 import { CONFIG_MAIN } from '../@core/routerConfig';
 import { IconCoreModule } from 'ngx-liburg-icon';
 import { ShellComponent } from './shell/shell.component';
+import {ToastrModule} from "ngx-toastr";
+import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
 
 @NgModule({
   declarations: [AppComponent, ShellComponent],
   imports: [
     BrowserModule,
-    RouterModule.forRoot(routes),
     FrameWholeModule.forRoot(CONFIG_MAIN),
+    RouterModule.forRoot(routes),
+    BrowserAnimationsModule,
+    ToastrModule.forRoot({
+      timeOut: 10000,
+      positionClass: 'toast-bottom-right',
+      preventDuplicates: true,
+    }),
     IconCoreModule,
   ],
   providers: [],
