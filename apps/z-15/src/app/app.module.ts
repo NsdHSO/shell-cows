9e59acda4b43c2a21e4f8966feb2884f992b1a41
NsdHSO (Nechiforel David-Samuel) <nechiforelsamuel@yahoo.com>
2022-08-25 00:00:20 +0300

    feat: asdasda

--- apps/z-15/src/app/app.module.ts
+++ apps/z-15/src/app/app.module.ts
@@ -1,31 +1,28 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { RouterModule } from '@angular/router';
+import { NgModule }                from '@angular/core';
+import { BrowserModule }           from '@angular/platform-browser';
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { RouterModule }            from '@angular/router';
+import { FrameWholeModule }        from 'ngx-liburg-frame-side';
+import { IconCoreModule }          from 'ngx-liburg-icon';
+import { CONFIG_MAIN }             from '../@core/routerConfig';
 
-import { AppComponent } from './app.component';
-import { routes } from './app.routing';
-import { FrameWholeModule } from 'ngx-liburg-frame-side';
-import { CONFIG_MAIN } from '../@core/routerConfig';
-import { IconCoreModule } from 'ngx-liburg-icon';
+import { AppComponent }   from './app.component';
+import { routes }         from './app.routing';
 import { ShellComponent } from './shell/shell.component';
-import {ToastrModule} from "ngx-toastr";
-import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
 
-@NgModule({
-  declarations: [AppComponent, ShellComponent],
-  imports: [
+@NgModule ( {
+  declarations : [
+    AppComponent,
+    ShellComponent
+  ],
+  imports      : [
     BrowserModule,
-    FrameWholeModule.forRoot(CONFIG_MAIN),
-    RouterModule.forRoot(routes),
+    FrameWholeModule.forRoot ( CONFIG_MAIN ),
+    RouterModule.forRoot ( routes ),
     BrowserAnimationsModule,
-    ToastrModule.forRoot({
-      timeOut: 10000,
-      positionClass: 'toast-bottom-right',
-      preventDuplicates: true,
-    }),
     IconCoreModule,
   ],
-  providers: [],
-  bootstrap: [AppComponent],
-})
+  providers    : [],
+  bootstrap    : [ AppComponent ],
+} )
 export class AppModule {}
